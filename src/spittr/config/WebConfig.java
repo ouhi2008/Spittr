fedee95a25993cd0ac492744bc6c9da0540aa32f
Fei Wang <fei.wang@morningstar.com>
2016-08-16 10:44:14 +0800

    Defining layout with Apache Tiles views

--- src/spittr/config/WebConfig.java
+++ src/spittr/config/WebConfig.java
@@ -11,21 +11,23 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("spittr.web")
 public class WebConfig extends WebMvcConfigurerAdapter{
-    @Bean
-    public ViewResolver viewResolver() { //配置JSP视图解析器
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        resolver.setViewClass(org.springframework.web.servlet.view.JstlView.class);
-        //可以在JSP页面中通过${}访问beans
-        resolver.setExposeContextBeansAsAttributes(true);
-        return resolver;
-    }
+//    @Bean
+//    public ViewResolver viewResolver() { //配置JSP视图解析器
+//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//        resolver.setPrefix("/WEB-INF/views/");
+//        resolver.setSuffix(".jsp");
+//        resolver.setViewClass(org.springframework.web.servlet.view.JstlView.class);
+//        //可以在JSP页面中通过${}访问beans
+//        resolver.setExposeContextBeansAsAttributes(true);
+//        return resolver;
+//    }
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
@@ -50,5 +52,18 @@ public class WebConfig extends WebMvcConfigurerAdapter{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 	    registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+	
+	@Bean
+	public TilesConfigurer tilesConfigurer(){
+		TilesConfigurer tiles = new TilesConfigurer();
+		tiles.setDefinitions(new String[] {"/WEB-INF/layout/tiles.xml"});
+		tiles.setCheckRefresh(true);
+		return tiles;
+	}
+	
+	@Bean
+	public ViewResolver viewResolver(){
+		return new TilesViewResolver();
 	}
 }
